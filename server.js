321409ede0293195a02e1fe0a705e771bcf1d894
Aaron Chesley <FlashBanistan66@gmail.com>
2016-08-19 14:33:42 -0600

    make it better

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 //Dependencies//
 var express = require('express');
-var serverConfig = require("./server_config.js");
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var port = serverConfig.serverPort;
-app.listen(process.env.PORT || port, function(){
+
+app.listen(process.env.PORT || 80, function(){
   console.log('Listening');
 })
