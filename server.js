80672b66ff5b5b4c93a2fa1ad96d055bb60cc4e5
Aaron Chesley <aaron.chesley1990@gmail.com>
2016-08-19 13:15:19 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 //Dependencies//
 var express = require('express');
-var serverConfig = require('./server_config.js');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var port = serverConfig.serverPort;
+var port = 80;
 app.listen(port, function(){
   console.log('Listening on port ', port);
 });
