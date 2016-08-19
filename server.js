262225aba5f7803e8d853ee2188e69c155120771
Aaron Chesley <FlashBanistan66@gmail.com>
2016-08-19 14:30:02 -0600

    Fixed server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Dependencies//
 var express = require('express');
+var serverConfig = require("./server_config.js");
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -7,4 +8,4 @@ app.use(express.static(__dirname + '/public'));
 var port = serverConfig.serverPort;
 app.listen(process.env.PORT || port, function(){
   console.log('Listening');
-
+})
